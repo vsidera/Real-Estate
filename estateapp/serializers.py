575d4585189c6d6d9317f2e55099454671f5d296
p-koskey <patiencekoskey@gmail.com>
2020-11-04 15:32:17 +0300

    create registration for new users

--- estateapp/serializers.py
+++ estateapp/serializers.py
@@ -2,6 +2,30 @@ from rest_framework import serializers
 from .models import Profile, Listing
 from django.contrib.auth.models import User
 
+class RegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type':'password'},write_only=True)
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password','password2']
+        extra_kwargs = {
+            'password': {'write_only':True}
+        }
+
+    def save(self):
+        user = User(
+            email=self.validated_data['email'],
+            username=self.validated_data['username']
+        )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({'password':'Passwords must match'})
+
+        user.set_password(password)
+        user.save()
+
+        return user
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
@@ -13,3 +37,4 @@ class ListingSerializer(serializers.ModelSerializer):
         model = Listing
         fields = ['id', 'Location', 'Price', 'Realtor', 'Image1', 'Image2', 'Image3', 'Image4','Bedrooms','Bathrooms']
 
+ 
