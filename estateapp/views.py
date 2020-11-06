fe8a1bd7d10df96dfb128f8ce8bc9ccfd4edbd03
Ronny <mwithiamoki@gmail.com>
2020-11-06 07:43:21 +0300

    aded class tour detail

--- estateapp/views.py
+++ estateapp/views.py
@@ -99,3 +99,38 @@ class BidView(APIView):
         if request.method == 'GET':
             serializer = BidSerializer(bid_post,many=True)
             return Response(serializer.data)
+class ToursView(APIView):
+    permission_classes = (IsAuthenticated,IsCompanyAdmin)
+    def get(self, request, format=None):
+        all_tours = Tours.objects.all()
+        serializers = ToursSerializer(all_tours, many=True)
+        return Response(serializers.data)
+    def post(self, request, format=None):
+        serializers = ToursSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+class ToursDetail(APIView):
+    permission_classes = (IsAuthenticated,IsCompanyAdmin)
+    def get_tour(self, pk):
+        try:
+            return Tours.objects.get(pk=pk)
+        except Tours.DoesNotExist:
+            return Http404
+    def get(self, request, pk, format=None):
+        tour = self.get_tour(pk)
+        serializers = ToursSerializer(tour)
+        return Response(serializers.data)
+    def put(self, request, pk, format=None):
+        tour = self.get_tour(pk)
+        serializers = ToursSerializer(tour, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk, format=None):
+        tour = self.get_tour(pk)
+        tour.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)            
