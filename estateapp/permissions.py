18b5eb7f208b90e99fb0b4ba8c9c764b73e4aa11
Ronny <mwithiamoki@gmail.com>
2020-11-06 08:51:00 +0300

    created permissions

--- estateapp/permissions.py
+++ estateapp/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import BasePermission
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 class IsCompanyAdmin(BasePermission):
     def has_permission(self, request, view):
