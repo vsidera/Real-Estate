3d54a67a9534eb259964ba71a1d5b63758572b2a
Ronny <mwithiamoki@gmail.com>
2020-11-06 07:44:41 +0300

    created class regiaterview in serializers

--- estateapp/serializers.py
+++ estateapp/serializers.py
@@ -1,15 +1,24 @@
-from rest_framework import serializers
-from .models import Profile, Listing
-from django.contrib.auth.models import User
-
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ['id','name', 'profile_picture', 'location']
-
-
-class ListingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Listing
-        fields = ['id', 'Location', 'Price', 'Realtor', 'Image1', 'Image2', 'Image3', 'Image4','Bedrooms','Bathrooms']
-
+from django.shortcuts import render
+from django.http  import HttpResponse
+from .serializers import RegistrationSerializer, ProfileSerializer, ListingSerializer, UserSerializer,BidSerializer,BidListingSerializer,BidAdminSerializer, ToursSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view,permission_classes
+from .models import Profile, Listing, Bid, User, Tours
+from .permissions import IsCompanyAdmin, IsNormalUser
+from rest_framework.permissions import IsAuthenticated
+# Create your views here.
+class RegisterView(APIView):
+    def post(self, request, format=None):
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            user = serializer.save()
+            data['response'] = "success registration"
+            data['email'] = user.email
+            data['username'] = user.username
+            data['role'] = user.role
+        else:
+            data = serializer.errors
+        return Response(data)
