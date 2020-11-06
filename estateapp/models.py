c436bc87c28411f54ee58f08bdb1b31c87b851f4
Ronny <mwithiamoki@gmail.com>
2020-11-06 07:50:01 +0300

    added class user

--- estateapp/models.py
+++ estateapp/models.py
@@ -1,56 +1,17 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth import get_user_model
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.conf import settings
+class User(AbstractUser):
+    USER_ROLES = (
+        ('CA', 'COMPANY ADMIN'),
+        ('NU', 'NORMAL USER')
+    )
+    role = models.CharField(
+        verbose_name='user role', max_length=2, choices=USER_ROLES,default='NU'
+    )
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    name = models.CharField(max_length=80, blank=True)
-    location = models.CharField(max_length=50, blank=True, null=True)
-    profile_picture = CloudinaryField('image')
     
-    def __str__(self):
-            return f'{self.user.username} profile'
-
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-            if created:
-                Profile.objects.create(user=instance)    
-
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-            instance.profile.save()        
-
-class Listing(models.Model):
-    ''' a model for Listing '''
-
-    Location = models.CharField(max_length=150)
-    Price = models.IntegerField()
-    Realtor = models.CharField(max_length=150)
-    Image1 = CloudinaryField('image')
-    Image2 = CloudinaryField('image')
-    Image3 = CloudinaryField('image')
-    Image4 = CloudinaryField('image')
-    Bedrooms = models.IntegerField()
-    Bathrooms = models.IntegerField()
-
-class Bid(models.Model):
-    ''' a model for Listing '''
-
-    Bidamount = models.IntegerField()
-    User = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='bidder')
-    Listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='listing')
-
-
-class Enquiry(models.Model):
-    ''' a model for Listing '''
-
-    Message = models.IntegerField()
-    User = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='enquirer')
-   
-
-        
-   
-
-
