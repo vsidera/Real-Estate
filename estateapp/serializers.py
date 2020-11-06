62c3a47d4fb1c05c7f5104054252018d317d1578
Ronny <mwithiamoki@gmail.com>
2020-11-06 07:47:50 +0300

    added tour details

--- estateapp/serializers.py
+++ estateapp/serializers.py
@@ -111,3 +111,27 @@ class ToursView(APIView):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ToursDetail(APIView):
+    permission_classes = (IsAuthenticated,IsCompanyAdmin)
+    def get_tour(self, pk):
+        try:
+            return Tours.objects.get(pk=pk)
+        except Tours.DoesNotExist:
+            return Http404
+    def get(self, request, pk, format=None):
+        tour = self.get_tour(pk)
+        serializers = ToursSerializer(tour)
+        return Response(serializers.data)
+    def put(self, request, pk, format=None):
+        tour = self.get_tour(pk)
+        serializers = ToursSerializer(tour, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk, format=None):
+        tour = self.get_tour(pk)
+        tour.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
